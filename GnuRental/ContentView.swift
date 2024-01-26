8cae60c5f0429b38027f3cd4f84fe4d0ad4c26d9
jjy <wjdwodud3525@gmail.com>
2024-01-26 15:52:01 +0900

    Initial Commit

---
+++ GnuRental/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GnuRental
+//
+//  Created by 정재영 on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GnuRental/GnuRentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GnuRentalApp.swift
+//  GnuRental
+//
+//  Created by 정재영 on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GnuRentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
